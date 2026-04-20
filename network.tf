794c1b4f522b0e0eb8b1936df1b8fb23e4c4769a
kumang subba <kumangsubba@gmail.com>
2026-04-20 23:38:32 +0300

    removed comments

--- network.tf
+++ network.tf
@@ -30,22 +30,6 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-
-# resource "aws_route_table_association" "public_assoc_b" {
-#   subnet_id      = aws_subnet.public_b.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-
-# resource "aws_route_table_association" "private_assoc_a" {
-#   subnet_id      = aws_subnet.private_a.id
-#   route_table_id = aws_route_table.private_rt.id
-# }
-
-# resource "aws_route_table_association" "private_assoc_b" {
-#   subnet_id      = aws_subnet.private_b.id
-#   route_table_id = aws_route_table.private_rt.id
-# }
-
 resource "aws_subnet" "public" {
   for_each = {
     a = "10.0.1.0/24"
